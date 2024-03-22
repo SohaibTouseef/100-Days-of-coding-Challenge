b6dda16e24e00d99de2b2002bdf780b172bea908
Sohaib Touseef <161127025+SohaibTouseef@users.noreply.github.com>
2024-03-22 20:04:29 +0500

    first commit

---
+++ Q1.ts
@@ -0,0 +1,7 @@
+// Starting of typescript journey with the installation og
+// 1.Vs code
+// 2.Node.js
+// 3.Typescrit
+
+
+// install [Node.js](https://nodejs.org/en/) , [Typescrit](https://www.typescriptlang.org/download) and [Vs code](https://code.visualstudio.com/) on your computer..
