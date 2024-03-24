f0d8fbe35a136743e63dc0584dfc9e01b3929991
Sohaib Touseef <161127025+SohaibTouseef@users.noreply.github.com>
2024-03-24 14:49:38 +0500

    Add files via upload

---
+++ Q68.js
@@ -0,0 +1,10 @@
+"use strict";
+// Question 68: Multiplying Decimals: Create a function that accepts two decimal numbers and returns their product. Round the result to two decimal places.
+// This function multiplies two decimal numbers
+function multiplyDecimals(num1, num2) {
+    // Multiplies the numbers and rounds the result to two decimal places
+    return Math.round((num1 * num2) * 100) / 100;
+}
+// Trying it with 0.1 and 0.2
+console.log(multiplyDecimals(0.1, 0.2)); // Shows 0.02
+// After multiplying, we round to make the result easier to read.
---
+++ Q68.ts
@@ -0,0 +1,12 @@
+
+// Question 68: Multiplying Decimals: Create a function that accepts two decimal numbers and returns their product. Round the result to two decimal places.
+
+// This function multiplies two decimal numbers
+function multiplyDecimals(num1: number, num2: number): number {
+    // Multiplies the numbers and rounds the result to two decimal places
+    return Math.round((num1 * num2) * 100) / 100;
+}
+
+// Trying it with 0.1 and 0.2
+console.log(multiplyDecimals(0.1, 0.2)); // Shows 0.02
+// After multiplying, we round to make the result easier to read.
